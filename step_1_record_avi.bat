8f6e010066038c2e20d50abdacfd35a6f77ce97f
cove <covecs@gmail.com>
2025-11-18 10:37:49 -0800

    add write up on how to do the capture and background info

--- step_1_record_avi.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo "Use VirtualDub to record the video from your VHS player to an AVI file."
-echo "Unfortunately scripting in VirtualDub is limited, so this step must be done manually."
