cbb11fd70963b4c250d246c5e7d89d3451336481
cove <covecs@gmail.com>
2025-11-23 11:04:40 -0800

    batch convert avi to ffv1

--- step_2_convert_avi_to_mkv.bat
+++ step_2_convert_avi_to_mkv.bat
@@ -19,29 +19,30 @@ if not exist "%FFMPEG%" (
     exit /b 1
 )
 
-echo Creating FFv1 archive "%BASENAME%_archive.mkv"...
-%FFMPEG% -nostdin -v error -i "%BASENAME%.avi" ^
-    -pix_fmt yuv422p ^
-    -color_primaries:v 6 -color_trc:v 6 -colorspace:v 5 -color_range:v 1 ^
-    -map 0:v:0 -c:v ffv1 ^
-        -level 3 ^
-        -g 1 ^
-        -coder 1 ^
-        -context 1 ^
-        -slices 24 ^
-        -slicecrc 1 ^
-    -map 0:a:0 -c:a pcm_s16le ^
-    -y "%BASENAME%_archive.mkv"
+REM Loop over every argument (each should be an .avi file)
+for %%I in (%*) do (
+    set "INPUT=%%~I"
+    set "FILENAME=%%~nxI"
+    set "BASENAME=%%~nI"
 
-@REM echo Creating mp4 proxy "%BASENAME%_proxy.mp4"...
-@REM %FFMPEG% -nostdin -v error -i "%BASENAME%_archive.mkv" ^
-@REM     -pix_fmt yuv422p ^
-@REM     -color_primaries:v 6 -color_trc:v 6 -colorspace:v 5 -color_range:v 1 ^
-@REM     -movflags +faststart ^
-@REM     -c:v libx265 -preset veryfast -crf 28 ^
-@REM     -c:a aac -b:a 41.1k -ac 1 -ar 48000 ^
-@REM     -metadata "title=Proxy for %BASENAME%_archive.mkv" ^
-@REM     -y "%BASENAME%_proxy.mp4"
+    echo Creating FFv1 archive "%%~nI_archive.mkv"...
+
+    "%FFMPEG%" -nostdin -v error -i "%%~I" ^
+        -pix_fmt yuv422p ^
+        -color_primaries:v 6 -color_trc:v 6 -colorspace:v 5 -color_range:v 1 ^
+        -map 0:v:0 -c:v ffv1 ^
+            -level 3 ^
+            -g 1 ^
+            -coder 1 ^
+            -context 1 ^
+            -slices 24 ^
+            -slicecrc 1 ^
+        -map 0:a:0 -c:a pcm_s16le ^
+        -y "%%~nI_archive.mkv"
+
+    echo Done with %%~I
+    echo.
+)
 
 echo Done.
 
