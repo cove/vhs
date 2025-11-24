a211dbfa2111f3540ed75c6cac111d76d5f59d40
cove <covecs@gmail.com>
2025-11-23 17:27:24 -0800

    fix

--- step_3_set_metadata.bat
+++ step_3_set_metadata.bat
@@ -1,2 +1,4 @@
 @echo off
-powershell.exe -NoExit -ExecutionPolicy Bypass -File "%~dp0step_3_set_metadata.ps1"
+powershell.exe -NoExit -ExecutionPolicy Bypass -File "%~dp0step_3_set_metadata.ps1" %*
+exit /b %errorlevel%
+
