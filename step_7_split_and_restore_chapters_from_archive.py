dc6030ba1a9f89d15aa483b93880e5592e8969bf
cove <covecs@gmail.com>
2025-11-30 11:35:50 -0800

    remove

--- step_7_split_and_restore_chapters_from_archive.py
+++ step_7_split_and_restore_chapters_from_archive.py
@@ -122,7 +122,6 @@ def main():
                 avs_script = f'''
 SetFilterMTMode("DEFAULT_MT_MODE", 2)
 SetMemoryMax(8192)
-LoadPlugin("{QTGMC_DIR}/DevIL.dll")
 LoadPlugin("{QTGMC_DIR}/DePanEstimate.dll")
 LoadPlugin("{QTGMC_DIR}/DePan.dll")
 LoadPlugin("{QTGMC_DIR}/ffms2.dll")
