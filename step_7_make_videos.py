5bcc4665923de894ce53ecdf167c0a2d0eb6c058
cove <covecs@gmail.com>
2025-12-01 15:17:00 -0800

    don't double frame rate

--- step_7_make_videos.py
+++ step_7_make_videos.py
@@ -139,7 +139,7 @@ Import("{QTGMC_DIR}/QTGMC.avsi")
 FFmpegSource2("{temp_raw.name}", atrack=-1)
 AssumeFPS(30000,1001)
 ConvertToYV12(matrix="Rec601")
-QTGMC(Preset="Very Slow",EZKeepGrain=1.0,Sharpness=1.2,SourceMatch=3,Lossless=2,TR2=3)
+QTGMC(Preset="Very Slow",FPSDivisor=2,FDEZKeepGrain=1.0,Sharpness=1.2,SourceMatch=3,Lossless=2,TR2=3)
 Crop(0,0,-2,-6)
 LanczosResize(640,480)
 {filter_avs}
