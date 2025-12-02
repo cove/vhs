20142018830a32f9087095e8a2d7a183cdd165b9
cove <covecs@gmail.com>
2025-12-01 18:41:41 -0800

    test wo crop

--- step_7_make_videos.py
+++ step_7_make_videos.py
@@ -158,7 +158,7 @@ FFmpegSource2("{temp_raw}", atrack=-1)
 AssumeFPS(30000,1001) 
 ConvertToYV12(matrix="Rec601") 
 QTGMC(Preset="Very Slow",FPSDivisor=2,EZKeepGrain=1.0,Sharpness=1.2,SourceMatch=3,Lossless=2,TR2=3) 
-Crop(0,0,-2,-6) 
+#Crop(0,0,-2,-6) 
 LanczosResize(640,480) 
 Prefetch()'''
         avs_file.write_text(avs_script, encoding="ascii")
