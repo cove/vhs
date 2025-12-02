813873775d0fae03ba297c279684a498fe5bc913
cove <covecs@gmail.com>
2025-12-01 20:29:56 -0800

    fix audio?

--- step_7_make_videos.py
+++ step_7_make_videos.py
@@ -214,7 +214,7 @@ Prefetch()'''
             ]
 
         cmd += [
-                "-map", "0:v", "-map_metadata", "-1",
+                "-map", "0:v", "-map", "0:a", "-map_metadata", "-1",
                 "-vf", f"ass={temp_ass.name}",
                 "-c:v", "libx265",
                 "-preset", "veryslow",
