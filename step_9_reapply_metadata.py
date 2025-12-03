f4ac860fe4658c25165158b842d7156b6e24d74b
cove <covecs@gmail.com>
2025-12-02 20:12:38 -0800

    reapply subtitles

--- step_9_reapply_metadata.py
+++ step_9_reapply_metadata.py
@@ -157,7 +157,7 @@ for src in all_videos:
     final_file = src
     temp_file = src.with_suffix(".subtitle_temp.mp4")
 
-    pprint.pprint(metadata_by_title)
+    pprint.pprint(metadata_by_title.keys())
     ffm, ch = load_metadata_for_video(src)
     if not ch:
         print(f"No chapters for {title}")
