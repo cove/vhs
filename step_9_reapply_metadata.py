dabfbbb4907b37042a86b1a6a8704bd6e2e7fa1f
cove <covecs@gmail.com>
2025-12-02 20:45:13 -0800

    reapply subtitles

--- step_9_reapply_metadata.py
+++ step_9_reapply_metadata.py
@@ -87,6 +87,7 @@ def ffprobe_metadata_field(path, key):
             "-of", "default=nw=1:nk=1",
             str(path)
         ], text=True).strip()
+        print(str(out))
         return out or ""
     except Exception:
         return ""
